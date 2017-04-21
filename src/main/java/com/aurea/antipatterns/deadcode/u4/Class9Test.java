04caa9a8e9089d288f7c9945d2ced8aa5644c09c
AlexanderRyabov <alexander.ryabov@aurea.com>
2017-04-21 15:13:13 +0300

    test sample

---
+++ src/main/java/com/aurea/antipatterns/deadcode/u4/Class9Test.java
@@ -0,0 +1,19 @@
+package com.aurea.antipatterns.deadcode.u4;
+
+//CHECKSTYLE:OFF
+public class Class9Test {
+    private void testMethod1(int abc, String used) {
+        System.out.println(used);
+    }
+
+    private void testMethod2(String used) {
+        System.out.println(used);
+    }
+
+    private void TestMethodA(String used) {
+        System.out.println(used);
+    }
+
+    public void Method3() {}
+}
+
